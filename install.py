f22a6c03bd2560325196a643bd3ba1f6cea9d926
antnux <103955741+antnux@users.noreply.github.com>
2022-05-30 12:50:16 +0200

    Update install.py

--- install.py
+++ install.py
@@ -6,5 +6,3 @@ os.system("cd tools/ && git clone https://github.com/Err0r-ICA/IP-Track/")
 os.system("cd tools/IP-Track && ")
 os.system("pip install colorama")
 os.system("python3 DDoS-toolkit.py")
-
-5
