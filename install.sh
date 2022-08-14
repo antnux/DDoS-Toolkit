b674902b5b944280536be808f2d1138a4fb50ff5
antnux <103955741+antnux@users.noreply.github.com>
2022-08-14 22:35:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,25 +7,25 @@ mkdir clone
 
 clear
 
-echo -e"\n [*]         searching GIT..."
+echo -e"\n [*]         searching GIT"
 
 if command -v git >/dev/null 2>&1 ; then
 
-echo -e "\n\n [+]             GIT FOUND!!!"
+echo -e "\n\n [+]             GIT FOUND"
 
 else
 echo -e "\n [-]          git not found"
-echo -e "\n\n [-]        INSTALLING git..."
+echo -e "\n\n [-]        INSTALLING git"
 
 sudo apt install git
 
 fi
 
-echo -e "\n\n\n [*]         searching python3..."
+echo -e "\n\n\n [*]         searching python3"
 
 if command -v python3 >/dev/null 2>&1 ; then
 
-echo -e "\n [+]        python3 FOUND!!!"
+echo -e "\n [+]        python3 FOUND"
 
 else 
 echo -e "\n [-]        python3 not found"
@@ -35,10 +35,10 @@ sudo apt install python3
 
 fi
 
-echo -e "\n\n\n [*]         searching python2..."
+echo -e "\n\n\n [*]         searching python2"
 
 if command -v python3 >/dev/null 2>&1 ; then
-echo -e "\n [+]        python2 FOUND!!!"
+echo -e "\n [+]        python2 FOUND"
 else
 echo -e "\n [-]        python2 not found"
 echo -e "\n [-]        installing python2"
@@ -47,11 +47,11 @@ sudo apt install python2||sudo pacman -S python2
 
 fi
 
-echo -e "\n\n\n [*]         searching pip3..."
+echo -e "\n\n\n [*]         searching pip3"
 
 if command -v pip3 >/dev/null 2>&1 ; then
 
-echo -e "\n [+]         pip3 FOUND!!!"
+echo -e "\n [+]         pip3 FOUND"
 
 else
 echo -e "\n [-]        pip3 not found"
@@ -65,15 +65,14 @@ echo -e "\n [+]         INSTALLING pip3..."
 
 sudo pip3 install bs4
 
-echo -e "\n [+]         INSTALLING pip3..."
+echo -e "\n [+]         INSTALLING pip3"
 
 sudo pip3 install requests
 pip3 install colorama
 pip3 install webbrowser
 
-echo -e "\n [+]         INSTALLING pip3..."
+echo -e "\n [+]         INSTALLING pip3"
 
-sudo pip3 install mechanize
 
 fi
 
@@ -88,7 +87,7 @@ fi
 if [ -d "$DR" ]; then
    echo -e "\n $FILE found."
    else
-   echo -e "\n [/]        CLONING DDoS-Ripper..."
+   echo -e "\n [/]        CLONING DDoS-Ripper"
    sudo git clone https://github.com/palahsu/DDoS-Ripper.git && mv $FILE tools
 
 fi
@@ -96,10 +95,8 @@ fi
 if [ -d "$DR" ]; then
    echo -e "\n $FILE found."
    else
-   echo -e "\n [/]        CLONING DDoS..."
+   echo -e "\n [/]        CLONING DDoS"
    sudo git clone https://github.com/Err0r-ICA/DDoS.git && mv $FILE tools
    python3 DDoS-Toolkit.py
-
-fi
 
 #credit lunarstone292#
