6e3aa927a89f4ce078b6b3449c629b4c28de081a
antnux <103955741+antnux@users.noreply.github.com>
2022-08-15 00:55:03 +0200

    Update install.py

--- install.py
+++ install.py
@@ -12,18 +12,29 @@ try:
     os.system("apt upgrade -y")
 
     print("INSTALLING PYTHON...")
-    os.system("apt install python2")
-    os.system("apt install python3-pip")
+    os.system("apt install python2 -y")
+    os.system("apt install python3-pip -y")
     
     print("INSTALLING NMAP...")
-    os.system("apt install nmap")
+    os.system("apt install nmap -y")
     
     print("INSTALLING PYTHON MODULES...")
     os.system("pip3 install colorama")
+    os.system("pip3 install certifi>=2019.11.28")
+    os.system("pip3 install chardet>=3.0.4")
+    os.system("pip3 install idna>=2.8")
+    os.system("pip3 install requests>=2.22.0")
+    os.system("pip3 install urllib3>=1.25.7")
 
+    
+    
     os.system("mkdir tools")
+    
     print("CLONING IP-TRACK...")
     os.system("git clone https://github.com/Err0r-ICA/IP-Track && mv IP-Track tools/")
+    
+    print("CLONING IPDump")
+    os.system("git clone https://github.com/adamdb5/IPDump && mv IPDump tools/")
 
     print("CLONING DDoS...")
     os.system("git clone https://github.com/Err0r-ICA/DDoS.git && mv DDoS tools/")
