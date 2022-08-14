4e3712657308f759be7074f473244a97d03d7353
antnux <103955741+antnux@users.noreply.github.com>
2022-08-14 22:24:16 +0200

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("mkdir tools")
-os.system("cd tools/ && git clone https://github.com/Err0r-ICA/DDoS.git")
-os.system("cd tools/ && git clone https://github.com/palahsu/DDoS-Ripper.git")
-os.system("cd tools/ && git clone https://github.com/Err0r-ICA/IP-Track/")
-os.system("cd tools/IP-Track && ")
-os.system("pip install colorama")
-os.system("python3 DDoS-toolkit.py")
